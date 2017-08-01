f2a2b35bbf63ce7f4cf4cb4e6b6f1937ed8cf555
Rein Chow <rein.chow@firstidea.vn>
2017-08-01 09:55:18 +0700

    Added IDeletable, IAuditable and IExtensible interfaces.

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -9,3 +9,40 @@ type BigSInt = string;
 declare interface IModelDTO {
 	id: BigSInt;
 }
+
+/**
+ * Represents a model that is never really removed from database.
+ */
+declare interface ISoftDeletable {
+	/**
+	 * If has value, this model is marked as deleted.
+	 * Otherwise, it is still active.
+	 */
+	deletedAt?: Date;
+}
+
+/**
+ * Represents a model whose history is tracked.
+ */
+declare interface IAuditable {
+	/**
+	 * The time when this model is created.
+	 */
+	createdAt: Date;
+
+	/**
+	 * The time when this model is last edited.
+	 */
+	updatedAt: Date;
+}
+
+/**
+ * Represents a model that can be added more properties.
+ */
+declare interface IExtensible {
+	/**
+	 * Contains additional properties.
+	 */
+	attributes?: any; // Should map to JSON type in PostreSQL.
+}
+
